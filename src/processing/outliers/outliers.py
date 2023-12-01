73a05cac97a12a6ec7f07f7d94bf96ff5a10db4e
iQuad <quentin.roels@gmail.com>
2023-12-01 10:42:57 +0100

    Added Simple Moving Average and Keras Anomaly Detection

---
+++ src/processing/outliers/outliers.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+def compute_outliers(ts1: pd.Series, ts2: pd.Series) -> pd.Series:
+    """
+    This function takes two time series as input and returns the outliers of the
+    :param ts1: The first time series
+    :param ts2: The second time series
+    :return: The outliers of the difference between the two time series
+    """
+
+    diff = ts1 - ts2
+    # Compute derivative of difference
+    diff = diff.diff()
+    # Absolute value of derivative
+    diff = diff.abs()
+
+    # Compute mean and standard deviation of derivative
+    mean = diff.mean()
+    std = diff.std()
+
+    # Compute outliers
+    outliers = diff[diff > mean + 3 * std]
+
+    return outliers
